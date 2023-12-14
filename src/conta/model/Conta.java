d35e3e45861ac2aff00ebbe8aed20c8d2adf6202
Teste teste <nathmoonz@hotmail.com>
2023-12-14 14:00:37 -0300

    Criei classe conta

---
+++ src/conta/model/Conta.java
@@ -0,0 +1,49 @@
+package conta.model;
+
+public class Conta {
+	private int numero;
+	private int agencia;
+	private int tipo;
+	private String titular;
+	private double saldo;
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+}
