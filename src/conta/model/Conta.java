dc95db619744eceacdf59e30bbbbe52e6cb6ab39
Teste teste <nathmoonz@hotmail.com>
2023-12-20 18:00:40 -0300

    Criei Classe ContaController

--- src/conta/model/Conta.java
+++ src/conta/model/Conta.java
@@ -46,4 +46,43 @@ public abstract class Conta {
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
+
+	public boolean sacar(float valor) {
+
+		if (this.getSaldo() < valor) {
+			System.out.println("Saldo Insuficiente!");
+			return false;
+		}
+
+		this.setSaldo(this.getSaldo() - valor);
+		return true;
+
+	}
+
+	public void depositar(float valor) {
+		this.setSaldo(this.getSaldo() + valor);
+	}
+
+	public void visualizar() {
+
+		String tipo = "";
+
+		switch (this.tipo) {
+		case 1:
+			tipo = "Conta Corrente";
+			break;
+		case 2:
+			tipo = "Conta Poupança";
+			break;
+		}
+
+		System.out.println("\n\n***********************************************************");
+		System.out.println("Dados da Conta:");
+		System.out.println("***********************************************************");
+		System.out.println("Numero da Conta: " + this.numero);
+		System.out.println("Agência: " + this.agencia);
+		System.out.println("Tipo da Conta: " + tipo);
+		System.out.println("Titular: " + this.titular);
+		System.out.println("Saldo: " + this.saldo);
+	}
 }
